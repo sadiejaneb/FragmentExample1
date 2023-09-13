dc95c738dbb69d26d689e4258a13fe723328cf9b
sadiejaneb <sadiejaneb@gmail.com>
2023-09-13 17:38:44 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FragmentExample1"
+include(":app")
+ 
